87174f58ce6f88c5843775d06503ef837b7645f9
Kumudu Gunasekara <Kumudu Gunasekara>
2016-08-21 04:30:26 +1000

    merge add_routing branch

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroService }  from './hero.service';
+import { routing }        from './app.routing';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
